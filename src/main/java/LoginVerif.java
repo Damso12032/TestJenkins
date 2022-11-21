263f304387449912cffbea798d883fb401f3b61b
Damso12032 <99388067+Damso12032@users.noreply.github.com>
2022-11-21 15:58:35 +0100

    Add files via upload

---
+++ src/main/java/LoginVerif.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+/**
+ * Servlet implementation class LoginVerif
+ */
+//@WebServlet("/LoginVerify")
+public class LoginVerif extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginVerif() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uname=request.getParameter("uname");
+		String pwd=request.getParameter("password");
+		if (uname.equals("Damso") && pwd.equals("admin")) {
+			System.out.println("les identifiants sont corrects\n");
+		}	
+		else System.out.println("Erreur dns les ids\n");	
+	}
+
+}
